a76f14e4e9eded337e87978f501224d21768f1ab
eternalsphere25 <eternalsphere25@gmail.com>
2022-10-23 17:38:15 +0900

    rewrite of code to incorporate OOP principles

--- clock.py
+++ clock.py
@@ -3,17 +3,21 @@
 # https://en.wikipedia.org/wiki/List_of_tz_database_time_zones
 #------------------------------------------------------------------------------
 
-
-import datetime
+import dateutil
 import holidays
-import zoneinfo
 import pyexcel_ods3
 import math
 import pycountry
+import tzlocal
+from datetime import datetime, time
+from dateutil.relativedelta import *
 from general_functions import *
-import geopy
-import timezonefinder
+from zoneinfo import ZoneInfo
+import timezone_diff_calc
 
+#-------------------------------------------------------------------------------
+# Classes
+#-------------------------------------------------------------------------------
 
 class DateTime:
     def __init__(self, input_datetime):
@@ -21,63 +25,78 @@ class DateTime:
         self.date = self.datetime.date()
         self.time = self.datetime.timetz().strftime("%H:%M:%S")
         self.tz = self.datetime.strftime("%z")
+        self.tz_name = self.datetime.strftime("%Z")
         self.dst_offset = self.datetime.dst()
 
-    def get_tz_abbrv(self, input_timezone):
-        if self.dst_offset.seconds == 0:
-            return self.tz_abbrv_dict[str(input_timezone)][0]
-        else:
-            return self.tz_abbrv_dict[str(input_timezone)][1]
-
     def calc_elapsed_time(self):
-        current_time = datetime.datetime.now().astimezone()
-        return (current_time - self.datetime)
+        time_now = get_datetime_now_with_timezone_local()
+        self.elapsed_time = relativedelta(time_now, self.datetime)
 
     def print_elapsed_time(self):
-        time_data = self.calc_elapsed_time()
-        weeks = math.floor(time_data.days/7)
-        days = time_data.days - (weeks*7)
-        hours = math.floor(time_data.seconds/3600)
-        minutes = math.floor((time_data.seconds - (hours*3600))/60)
-        seconds = time_data.seconds - (hours*3600) - (minutes*60)
-        return (f'{weeks} Weeks, {days} Days, {hours} Hours, '
-                f'{minutes} Minutes, {seconds} Seconds')
-
-    tz_abbrv_dict = {
-    "America/New_York": ('EST', 'EDT'),
-    "America/Los_Angeles": ('PST', 'PDT'),
-    "Asia/Tokyo": ('JST', 'JST'),
-    "Asia/Manila": ('PST', 'PST'),
-    "Asia/Ulaanbaatar": ('MST', 'MST'),
-    "Europe/Amsterdam": ('CET', 'CEST'),
-    "Asia/Singapore": ('SST', 'SST'),
-    "Asia/Shanghai": ('CST', 'CST'),
-    "Australia/Sydney": ('AEST', 'AEDT'),
-    }
-
-class WorldTimeZoneInfo:
-    def __init__(self):
-        self.local = self.tz_IATA[
-                        str(datetime.datetime.now().astimezone().tzinfo)]
-        self.new_york = zoneinfo.ZoneInfo("America/New_York")
-        self.los_angeles = zoneinfo.ZoneInfo("America/Los_Angeles")
-        self.tokyo = zoneinfo.ZoneInfo("Asia/Tokyo")
-        self.manila = zoneinfo.ZoneInfo("Asia/Manila")
-        self.ulaanbaatar = zoneinfo.ZoneInfo("Asia/Ulaanbaatar")
-        self.amsterdam = zoneinfo.ZoneInfo("Europe/Amsterdam")
-        self.singapore = zoneinfo.ZoneInfo("Asia/Singapore")
-        self.shanghai = zoneinfo.ZoneInfo("Asia/Shanghai")
-        self.sydney = zoneinfo.ZoneInfo("Australia/Sydney")
-
-    tz_IATA = {"Tokyo Standard Time": 'Asia/Tokyo'}
+        print(f'{self.elapsed_time.years} Years, '
+              f'{self.elapsed_time.months} Months, '
+              f'{self.elapsed_time.days} Days, '
+              f'{self.elapsed_time.hours} Hours, '
+              f'{self.elapsed_time.minutes} Minutes, '
+              f'{self.elapsed_time.seconds} Seconds')
 
 
+class Person:
+    def __init__(self, input_data_list, input_name):
+        self.name = input_name
+        self.age_data = extract_single_event_data(input_data_list, input_name)
+        self.age_datetime = build_datetime_object_from_components(self.age_data)
+
+    def calculate_age(self):
+        time_now = get_datetime_now_with_timezone_local()
+        self.current_age = relativedelta(time_now, self.age_datetime)
+
+    def print_age(self):
+        if self.name == 'You':
+            verb = 'are'
+        else:
+            verb = 'is'
+        print(f'\n{self.name} {verb} currently '
+              f'{self.current_age.years} years, '
+              f'{self.current_age.months} months, '
+              f'{self.current_age.days} days, '
+              f'{self.current_age.hours} hours, '
+              f'{self.current_age.minutes} minutes, '
+              f'{self.current_age.seconds} seconds, and '
+              f'{self.current_age.microseconds} microseconds old')
+           
+
+class TimeDataImport:
+    def __init__(self, input_file_location):
+        self.event_past_raw = pyexcel_ods3.get_data(
+                                input_file_location)['events_past']
+        self.event_future_raw = pyexcel_ods3.get_data(
+                                input_file_location)['events_future']
+        self.birthday_raw = pyexcel_ods3.get_data(
+                            input_file_location)['birthdays']
+        self.saved_locations_raw = pyexcel_ods3.get_data(
+                                    input_file_location)['saved_locations']
+
+    def clean_input_data(self):
+        self.event_past_clean = [
+            row for row in self.event_past_raw if len(row) != 0]
+        self.event_future_clean = [
+            row for row in self.event_future_raw if len(row) != 0]
+        self.birthday_clean = [
+            row for row in self.birthday_raw if len(row) != 0]
+        self.saved_locations_clean = [
+            row for row in self.saved_locations_raw if len(row) != 0]
+
+
+#-------------------------------------------------------------------------------
+# Functions - Event Info Extraction
+#-------------------------------------------------------------------------------
 
 def build_datetime_object_from_components(input_list):
-    date = datetime.datetime.strptime(input_list[1], f"%Y/%m/%d").date()
+    date = datetime.strptime(input_list[1], f"%Y/%m/%d").date()
     time = input_list[2]
-    tz = zoneinfo.ZoneInfo(input_list[3])
-    return datetime.datetime.combine(date, time, tzinfo=tz)
+    tz = ZoneInfo(input_list[3])
+    return datetime.combine(date, time, tzinfo=tz)
 
 def extract_single_event_data(input_data, search_str):
     for x in range(len(input_data)):
@@ -86,11 +105,80 @@ def extract_single_event_data(input_data, search_str):
             output_list = selected_row
     return output_list
 
+
+#-------------------------------------------------------------------------------
+# Functions - Age Calculations for People
+#-------------------------------------------------------------------------------
+
+def calc_age_diff_two_people(person1, person2):
+    time_diff = person1.current_age - person2.current_age
+    older_younger = person1.age_datetime - person2.age_datetime
+    return time_diff, older_younger
+
+def print_age_diff_two_people(input_calc_age_diff, person1, person2):
+    if input_calc_age_diff[1].total_seconds() == 0:
+        print(f'{person1.name} and {person2.name} are the same age')
+    else:
+        #Determine 'older' or 'younger' message
+        if input_calc_age_diff[1].total_seconds() < 0:
+            old_young = 'older'
+        elif input_calc_age_diff[1].total_seconds() > 0:
+            old_young = 'younger'
+        #Determine proper verb for first person
+        if person1.name == 'You':
+            verb = 'are'
+        else:
+            verb = 'is'
+        #Print age difference
+        print(f'\n{person1.name} {verb} '
+              f'\n{input_calc_age_diff[0].years} years, '
+              f'{input_calc_age_diff[0].months} months, '
+              f'{input_calc_age_diff[0].days} days, '
+              f'{input_calc_age_diff[0].hours} hours, '
+              f'{input_calc_age_diff[0].minutes} minutes, '
+              f'{input_calc_age_diff[0].seconds} seconds, and '
+              f'{input_calc_age_diff[0].microseconds} microseconds '
+              f'\n{old_young} than {person2.name}')
+
+
+#-------------------------------------------------------------------------------
+# Functions - Holidays
+#-------------------------------------------------------------------------------
+
+def print_country_holidays(country, year):
+    holiday_dict = holidays.country_holidays(country, years=year)
+    country_info = pycountry.countries.get(alpha_2=country)
+    print(f'\n{country_info.name} Holidays in {year}:')
+    print_dict_line_by_line(holiday_dict)
+
+def print_time_until_holiday(locale, year, name):
+    event_date = find_holiday_date_from_name(locale, year, name)
+    if event_date != None:
+        event_datetime = datetime.combine(
+                            event_date, time(0,0,0))
+        difference = relativedelta(event_datetime, datetime.now())
+        print(f'\nTime until {name}: '
+              f'\n{difference.years} Years, '
+              f'{difference.months} Months, '
+              f'{difference.days} Days, '
+              f'{difference.hours} Hours, '
+              f'{difference.minutes} Minutes, '
+              f'{difference.seconds} Seconds')
+
+
+#-------------------------------------------------------------------------------
+# Functions
+#-------------------------------------------------------------------------------
+
+def clean_input_ods_raw_data(input_raw_data):
+    return [row for row in input_raw_data if len(row) != 0]
+
 def find_holiday_date_from_name(country, year, name):
     list_of_holidays = holidays.country_holidays(country, years=year)
     if len(list_of_holidays.get_named(name)) == 0:
         country_info = pycountry.countries.get(alpha_2=country)
-        print(f'\nWARNING: "{name}" is not celebrated in {country_info.name}'
+        print(f'\nWARNING: "{name}" is/may not (be) celebrated in '
+              f'{country_info.name}'
               f'\n\t  Perhaps it goes by a different local name?'
               f'\n\t  Did you make sure you spelled "{name}" correctly?')
         return None
@@ -104,67 +192,8 @@ def find_holiday_date_from_name(country, year, name):
         else:
             return holiday_date[0]
 
-def find_time_diff_between_two_locations():
-    #Find time difference between two cities/towns/villages/hamlets
-    loc1 = input('\nEnter first city/town/village/hamlet name: ')
-    loc2 = input('Enter second city/town/village/hamlet name: ')
-
-    #Confirm input
-    print(f'\nConfirmation: \nLocation #1: {loc1} \nLocation #2: {loc2}')
-    choice = input('\nProceed? y/n ')
-
-    if choice == 'n':
-        print('Input cancelled')
-    elif choice == 'y':
-        #Initialize objects, one for each location
-        tz1 = zoneinfo.ZoneInfo(find_timezone_from_location_name(loc1))
-        tz2 = zoneinfo.ZoneInfo(find_timezone_from_location_name(loc2))
-
-        if tz1 != None and tz2 != None:
-            #Print the date and time at both locations
-            loc1_time = DateTime(datetime.datetime.now().astimezone(tz1))
-            loc2_time = DateTime(datetime.datetime.now().astimezone(tz2))
-            print(f'\nCurrent Time in {loc1}:'
-                f'\n\t{loc1_time.date} {loc1_time.time}')
-            print(f'Current Time in {loc2}:'
-                f'\n\t{loc2_time.date} {loc2_time.time}')     
-
-            #Calculate time difference
-            print('\nCalculating time difference...')
-            loc1_offset = int(loc1_time.tz)/100
-            loc2_offset = int(loc2_time.tz)/100
-            time_difference = (loc1_offset - loc2_offset)
-
-            #Print time difference
-            if time_difference > 0:
-                print(f'{loc1} is {time_difference:.2f} hour(s) ahead of '
-                      f'{loc2}')
-            elif time_difference < 0:
-                print(f'{loc1} is {time_difference:.2f} hour(s) behind {loc2}')
-            else:
-                print(f'There is no time difference between {loc1} and {loc2}')
-
-def find_timezone_from_location_name(input_loc):
-    #Set up geolocation service
-    geolocator = geopy.geocoders.Nominatim(
-                    user_agent='personal_timezone_project')
-
-    #Pull location data of the input city/town/village/hamlet
-    loc_data = geolocator.geocode(input_loc)
-
-    if loc_data == None:
-        print(f'ERROR: {input_loc} does not exist!')
-    else:
-        #Find timezone based on latitude and longitude coordinates
-        tz_finder = timezonefinder.TimezoneFinder()
-        tz_result = tz_finder.timezone_at(
-                        lng = loc_data.longitude, lat=loc_data.latitude)
-        
-        print(f'\nLocation Search Result:\n{loc_data}\nLat: {loc_data.latitude} Long: {loc_data.longitude}\nTime Zone: {tz_result}')
-
-        return tz_result
-
-def format_timedelta_to_readable_string(input_timedelta):
+"""
+def format_timedelta_object_to_readable_string(input_timedelta):
     time_data = input_timedelta
     weeks = math.floor(time_data.days/7)
     days = time_data.days - (weeks*7)
@@ -173,85 +202,60 @@ def format_timedelta_to_readable_string(input_timedelta):
     seconds = time_data.seconds - (hours*3600) - (minutes*60)
     return (f'{weeks} Weeks, {days} Days, {hours} Hours, '
             f'{minutes} Minutes, {seconds} Seconds')    
+"""
 
-def print_all_saved_locations():
-    #Get system time zone
-    print_current_time('Local Time', zoneinfo.ZoneInfo(tz.local))
-    print_current_time('Tokyo, JP', tz.tokyo)
-    print_current_time('New York, NY', tz.new_york)
-    print_current_time('Los Angeles, CA', tz.los_angeles)
-    print_current_time('Ulaanbaatar, MN', tz.ulaanbaatar)
-    print_current_time('Manila, PH', tz.manila)
-    print_current_time('Amsterdam, NL', tz.amsterdam)
-    print_current_time('Singapore, SG', tz.singapore)
-    print_current_time('Shanghai, CN', tz.shanghai)
-    print_current_time('Sydney, NSW', tz.sydney)
+def get_datetime_now_with_timezone_local():
+    return datetime.now(dateutil.tz.gettz(str(tzlocal.get_localzone())))
 
-def print_country_holidays(country, year):
-    holiday_dict = holidays.country_holidays(country, years=year)
-    country_info = pycountry.countries.get(alpha_2=country)
-    print(f'\n{country_info.name} Holidays in {year}:')
-    print_dict_line_by_line(holiday_dict)
+def print_all_saved_locations(input_location_data):
+    for item in range(len(input_location_data)):
+        if item == 0:
+            print_current_time('Local Time', tzlocal.get_localzone())
+        else:
+            current_item = input_location_data[item]
+            location_name = f'{current_item[0]}, {current_item[1]}'
+            timezone_name = current_item[2]
+            print_current_time(location_name, ZoneInfo(timezone_name))
 
 def print_current_time(location, timezone):
     #Get current time
-    current_datetime = DateTime(datetime.datetime.now().astimezone(timezone))
+    current_datetime = DateTime(datetime.now().astimezone(timezone))
 
     #Print out location, date, time, and DST offset
-    print(f'\nLocation: {location}')
-    print(f'Today\'s date is: '
-        f'{current_datetime.date}')
     if current_datetime.dst_offset.seconds == 0:
-        print(f'The time right now is: '
-            f'{current_datetime.time} {current_datetime.tz} '
-            f'({current_datetime.get_tz_abbrv(timezone)})')
-        print('DST currently not in effect')
+        dst_status = 'DST is currently not in effect'
     else:
-        print(f'The time right now is: '
-            f'{current_datetime.time} {current_datetime.tz} '
-            f'({current_datetime.get_tz_abbrv(timezone)})')
-        print(f'DST offset: {current_datetime.dst_offset}')
+        dst_status = f'DST offset: {current_datetime.dst_offset}'
+    print(f'\nLocation: {location}'
+          f'\nToday\'s date is: '
+          f'{current_datetime.date}'
+          f'\nThe time right now is: '
+          f'{current_datetime.time} {current_datetime.tz} '
+          f'({current_datetime.tz_name}) \n{dst_status}')
 
 def print_elapsed_time_multiple(input_data_list):
     for x in range(len(input_data_list)):
-        if x == 0:
-            pass
-        else:
-            selected_row = input_data_list[x]
-            event = DateTime(
-                build_datetime_object_from_components(selected_row))
-            print(f'\nTime elapsed since {selected_row[0]}: '
-                f'\n{event.print_elapsed_time()}')
+        if x != 0:
+            print_elapsed_time_single(input_data_list[x])
 
 def print_elapsed_time_single(input_data_list):
     event = DateTime(
         build_datetime_object_from_components(input_data_list))
-    print(f'\nTime elapsed since {input_data_list[0]}: '
-        f'\n{event.print_elapsed_time()}')
+    print(f'\nTime elapsed since {input_data_list[0]}: ')
+    event.calc_elapsed_time()
+    event.print_elapsed_time()
 
 def print_time_have_been_for(input_data_list, input_str):
     event = DateTime(
         build_datetime_object_from_components(input_data_list))
-    print(f'\n{input_str[0]} have been {input_str[1]} for: '
-        f'\n{event.print_elapsed_time()}')
+    print(f'\n{input_str[0]} have been {input_str[1]} for: ')
+    event.calc_elapsed_time()
+    event.print_elapsed_time()
 
-def print_time_until_holiday(locale, year, name):
-    event_date = find_holiday_date_from_name(locale, year, name)
-    if event_date != None:
-        event_datetime = datetime.datetime.combine(
-                            event_date, datetime.time(0,0,0))
-        difference = event_datetime - datetime.datetime.now()
-        print(f'\nTime until {name}: '
-              f'\n{format_timedelta_to_readable_string(difference)}')
-    else:
-        pass
 
 #-------------------------------------------------------------------------------
 # 0. Global Definitions
 #-------------------------------------------------------------------------------
-
-#Generate world time zone object
-tz = WorldTimeZoneInfo()
 
 #Define event/birthday info file
 event_time_info = ("D:\その他\Python Scripts\info_files\clock_info.ods")
@@ -261,25 +265,24 @@ event_time_info = ("D:\その他\Python Scripts\info_files\clock_info.ods")
 # 1. Driver Code
 #-------------------------------------------------------------------------------
 
-#Display every clock saved in memory
-print_all_saved_locations()
+#Import event/birthday/location etc information and clean the input
+timedata = TimeDataImport(event_time_info)
+timedata.clean_input_data()
 
-#Grab event/birthday information
-event_past_data_raw = pyexcel_ods3.get_data(event_time_info)['events_past']
-event_future_data_raw = pyexcel_ods3.get_data(event_time_info)['events_future']
-birthday_data_raw = pyexcel_ods3.get_data(event_time_info)['birthdays']
+#Display every clock saved in memory
+print_all_saved_locations(timedata.saved_locations_clean)
 
 #Print all past event time
-print_elapsed_time_multiple(event_past_data_raw)
+print_elapsed_time_multiple(timedata.event_past_clean)
 
 #Grab specific birthday information
-birthday_info = extract_single_event_data(birthday_data_raw, 'You')
+birthday_info = extract_single_event_data(timedata.birthday_clean, 'You')
 
 #Print single event
 print_time_have_been_for(birthday_info, ['You', 'alive'])
 
 california_free = extract_single_event_data(
-    event_past_data_raw, 'Last Time in California')
+    timedata.event_past_clean, 'Last Time in California')
 print_time_have_been_for(california_free, ['You', 'California-free'])
 
 
@@ -291,4 +294,24 @@ print_time_until_holiday('US', 2023, "New Year's")
 
 print_time_until_holiday('JP', 2022, '勤労感謝の日')
 
-find_time_diff_between_two_locations()
+#find_time_diff_between_two_locations()
+
+#Sample code for person age calculations
+mom = Person(timedata.birthday_clean, 'Mom')
+mom.calculate_age()
+mom.print_age()
+
+you = Person(timedata.birthday_clean, 'You')
+you.calculate_age()
+you.print_age()
+
+
+age_diff = calc_age_diff_two_people(mom, you)
+print_age_diff_two_people(age_diff, mom, you)
+
+age_diff = calc_age_diff_two_people(you, mom)
+print_age_diff_two_people(age_diff, you, mom)
+
+
+
+
--- timezone_diff_calc.py
+++ timezone_diff_calc.py
@@ -5,44 +5,35 @@
 #
 # Changelog
 #
+# Version 1.1
+# 10/23/2022
+# + Changes
+#   - Rewrote some code to make functions more easily callable by other programs
+#   - Removed superfluous code
+#
 # Version 1.0
 # 10/11/2022
 # + New Features
 #   - Wrote the program
 #------------------------------------------------------------------------------
 
-import datetime
 import geopy
-import math
-import timezonefinder
+from timezonefinder import TimezoneFinder
 import zoneinfo
+from datetime import *
 
 
 #-------------------------------------------------------------------------------
 # Classes
 #-------------------------------------------------------------------------------
 
-class DateTime:
+class DateTimeTZ:
     def __init__(self, input_datetime):
         self.datetime = input_datetime
         self.date = self.datetime.date()
         self.time = self.datetime.timetz().strftime("%H:%M:%S")
         self.tz = self.datetime.strftime("%z")
         self.dst_offset = self.datetime.dst()
-
-    def calc_elapsed_time(self):
-        current_time = datetime.datetime.now().astimezone()
-        return (current_time - self.datetime)
-
-    def print_elapsed_time(self):
-        time_data = self.calc_elapsed_time()
-        weeks = math.floor(time_data.days/7)
-        days = time_data.days - (weeks*7)
-        hours = math.floor(time_data.seconds/3600)
-        minutes = math.floor((time_data.seconds - (hours*3600))/60)
-        seconds = time_data.seconds - (hours*3600) - (minutes*60)
-        return (f'{weeks} Weeks, {days} Days, {hours} Hours, '
-                f'{minutes} Minutes, {seconds} Seconds')
 
 
 #-------------------------------------------------------------------------------
@@ -67,8 +58,8 @@ def find_time_diff_between_two_locations():
 
         if tz1 != None and tz2 != None:
             #Print the date and time at both locations
-            loc1_time = DateTime(datetime.datetime.now().astimezone(tz1))
-            loc2_time = DateTime(datetime.datetime.now().astimezone(tz2))
+            loc1_time = DateTimeTZ(datetime.now().astimezone(tz1))
+            loc2_time = DateTimeTZ(datetime.now().astimezone(tz2))
             print(f'\nCurrent Time in {loc1}:'
                 f'\n\t{loc1_time.date} {loc1_time.time}')
             print(f'Current Time in {loc2}:'
@@ -80,14 +71,7 @@ def find_time_diff_between_two_locations():
             loc2_offset = int(loc2_time.tz)/100
             time_difference = (loc1_offset - loc2_offset)
 
-            #Print time difference
-            if time_difference > 0:
-                print(f'{loc1} is {time_difference:.2f} hour(s) ahead of '
-                      f'{loc2}')
-            elif time_difference < 0:
-                print(f'{loc1} is {time_difference:.2f} hour(s) behind {loc2}')
-            else:
-                print(f'There is no time difference between {loc1} and {loc2}')
+            return loc1, loc2, time_difference
 
 def find_timezone_from_location_name(input_loc):
     #Set up geolocation service
@@ -101,17 +85,32 @@ def find_timezone_from_location_name(input_loc):
         print(f'ERROR: {input_loc} does not exist!')
     else:
         #Find timezone based on latitude and longitude coordinates
-        tz_finder = timezonefinder.TimezoneFinder()
+        tz_finder = TimezoneFinder()
         tz_result = tz_finder.timezone_at(
                         lng = loc_data.longitude, lat=loc_data.latitude)
-        
-        print(f'\nLocation Search Result:\n{loc_data}\nLat: {loc_data.latitude} Long: {loc_data.longitude}\nTime Zone: {tz_result}')
-
+        print(f'\nLocation Search Result:\n{loc_data}'
+              f'\nLat: {loc_data.latitude} Long: {loc_data.longitude}'
+              f'\nTime Zone: {tz_result}')
         return tz_result
+
+def print_timezone_diff(input_time_data_object):
+    loc1 = input_time_data_object[0]
+    loc2 = input_time_data_object[1]
+    time_difference = input_time_data_object[2]
+
+    if time_difference > 0:
+        print(f'{loc1} is currently {time_difference:.2f} hour(s) ahead of '
+                f'{loc2}')
+    elif time_difference < 0:
+        print(f'{loc1} is currently {time_difference:.2f} hour(s) behind '
+                f'{loc2}')
+    else:
+        print(f'There is no time difference between {loc1} and {loc2}')
 
 
 #-------------------------------------------------------------------------------
 # Driver Code - Uncomment below to run directly
 #-------------------------------------------------------------------------------
 
-find_time_diff_between_two_locations()
+#demo = find_time_diff_between_two_locations()
+#print_timezone_diff(demo)
